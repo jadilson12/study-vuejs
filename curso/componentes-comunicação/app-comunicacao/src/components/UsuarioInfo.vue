59747606e5a44052aa6974ca9b343504bf97ad97
Jadilson Guedes <jadilson12@gmail.com>
2019-07-17 10:04:12 -0300

    Adiciona app-cli comunicação entre componentes

---
+++ curso/componentes-comunicação/app-comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p> 
+        <p>Nome do Usuário: <strong>{{ enverterNome() }}</strong></p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nome'],
+    methods: {
+        enverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>

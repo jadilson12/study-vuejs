ff4e114976c3af967e5f13022d317d6a35e0ea64
Jadilson Guedes <jadilson12@gmail.com>
2019-07-17 10:03:22 -0300

    Adiciona componente-intro

---
+++ curso/componentes/componente-intro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <!-- <contador></contador> -->
+   <contadores></contadores>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+        contador: 0
+    }
+  }
+}
+</script>
---
+++ curso/componentes/componente-intro/src/components/Contadores.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="contadores">
+         <h1>Contadores</h1>
+        <contador></contador>
+        <button>?</button>
+    </div>
+</template>
+
+<script>
+import Contador from './contador.vue'
+export default {
+    components: {'contador': Contador }
+}
+</script>
+
+<style>
+
+</style>

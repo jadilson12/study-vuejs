448911ebf3146d267a8ebd80207e1119d1511ec8
Jadilson Guedes <jadilson12@gmail.com>
2019-07-18 09:31:09 -0300

    Atualizando componentes de comunicacão

--- curso/componentes-comunicação/app-comunicacao/src/components/UsuarioInfo.vue
+++ curso/componentes-comunicação/app-comunicacao/src/components/UsuarioInfo.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="componente">
         <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p> 
-        <p>Nome do Usuário: <strong>{{ enverterNome() }}</strong></p>
-
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome (Callback)</button>
     </div>
 </template>
 
 <script>
+import barramento from '@/barramento'
+
 export default {
-    props: ['nome'],
+    props: {
+        nome: {
+            type: String,
+            // required: true,
+            // default: function() {
+            //     return Array(10).fill(0).join(',')
+            // }
+            default: 'Anônimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
     methods: {
-        enverterNome() {
+        inverterNome() {
             return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            // this.nome = 'Pedro'
+            this.$emit('nomeMudou', 'Pedro')
         }
     },
+    created() {
+        barramento.quandoIdadeMudar(idade => {
+            this.idade = idade
+        })
+    }
 }
 </script>
 

9bd00cf17f6e9cf57822e069f4d0916c83994438
Jadilson Guedes <jadilson12@gmail.com>
2019-10-11 16:27:58 -0300

    add stack-trader

---
+++ curso/stock-trader/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+   <div>
+       <h1 class="display-3 font-weight-light mb-4">Negocia e consulte suas os ações</h1>
+       <v-sheet :elevation="7" class="pa-2" color="primary">
+           <v-icon class="white--text mr-3">info </v-icon>
+           <span class="headline white--text font-weight-light">Você pode salvar & Carregar os dados</span>
+       </v-sheet >
+        <v-sheet :elevation="7" class="pa-2 success darken-1 mt-3" color="primary">
+           <v-icon class="white--text mr-3">info </v-icon>
+           <span class="headline white--text font-weight-light">
+               Clique em 'Finalizar Dia' paa iniciar novo dia! </span>
+       </v-sheet >
+       <v-divider class="my-4"/>
+       <p class="display-1"><strong>Seu Saldo:</strong> {{ funds | currency }}</p>
+
+   </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ curso/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: "BMW", price: 110 },
+    { id: 2, name: "GM", price: 200 },
+    { id: 3, name: "FIAT", price: 510 },
+    { id: 4, name: "Google", price: 50 }
+]
